9c1df79d7bfa8f48b40a93a01b982ccfc931bb19
lfpimenta <lfpimenta@gmail.com>
2020-04-05 18:14:57 +0100

    test CI

--- src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -3,6 +3,9 @@
  */
 package org.pictolearn.docker;
 
+import java.net.MalformedURLException;
+import java.util.Random;
+
 /**
  * @author Luis.Pimenta
  *
@@ -11,9 +14,16 @@ public class HelloWorldPing {
 
 	/**
 	 * @param args
+	 * @throws MalformedURLException 
 	 */
-	public static void main(String[] args) {
-		for(int i = 0 ; i < 100; i++){
+	public static void main(String[] args) throws MalformedURLException {
+		Random r = new Random();
+		
+		int n = r.nextInt(100) + 50;
+		
+		
+		
+		for(int i = 0 ; i < n; i++){
 			System.out.println("Hello World Pinging nr: " + i );
 			try {
 				Thread.sleep(1000);
